3746fa406f33699fece3515822609c5eee95eb33
GusRodrigues <omykronbr@gmail.com>
2019-11-24 15:28:33 -0500

    Input parser and test cases

---
+++ Assignment5/Helper/Parser.cs
@@ -0,0 +1,87 @@
+﻿/* Assignment 5
+ * 
+ * Revision History
+ *      Gustavo Bonifacio Rodrigues, 2019.11.23: Created
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5.Helper
+{
+    /// <summary>
+    /// Parser is a helper class that will filter users inputs that program can handle.
+    /// </summary>
+    public static class Parser
+    {
+        /// <summary>
+        /// Parse the user input to lower case.
+        /// </summary>
+        /// <param name="input">The user input.</param>
+        /// <returns>The user input in lower case.</returns>
+        /// <exception cref="ArgumentNullException">If input is null or whitespaced.</exception>
+        public static string ParseInputToString(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            return input.ToLower();
+        }
+
+        /// <summary>
+        /// Convert string to int if non-negative and digit only.
+        /// </summary>
+        /// <param name="input">The user input.</param>
+        /// <returns>The int represented by the user.</returns>
+        /// <exception cref="FormatException">If input is not digit.</exception>
+        public static int ParseToInt(string input)
+        {
+            if (input.Length > 11)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!char.IsDigit(input[i]))
+                {
+                    throw new FormatException();
+                }
+            }
+
+            return int.Parse(input);
+        }
+
+        /// <summary>
+        /// A filtered input is checked for the menu options.
+        /// </summary>
+        /// <param name="input">The user input</param>
+        /// <returns>int representing the menu selection.</returns>
+        public static int ParseInputToMenu(string input)
+        {
+            // first item
+            if (input.Equals("1") || input.Equals("a") || input.StartsWith("1") || input.StartsWith("create") || input.StartsWith("new") || input.StartsWith("a-"))
+            {
+                return 1;
+            }
+            else if (input.Equals("2") || input.Equals("b") || input.StartsWith("2") || input.StartsWith("existing") || input.StartsWith("display") || input.StartsWith("b-"))
+            {
+                return 2;
+            }
+            else if (input.Equals("3") || input.Equals("c") || input.StartsWith("3") || input.StartsWith("edit") || input.StartsWith("change") || input.StartsWith("c-"))
+            {
+                return 3;
+            }
+            else if (input.Equals("4") || input.Equals("exit") || input.Equals("bye") || input.Equals("adios") || input.StartsWith("4") || input.StartsWith("four") || input.StartsWith("exit") || input.StartsWith("d-") || input.Equals("d"))
+            {
+                return 4;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ TestsAssignment5/Helper/ParserTest.cs
@@ -0,0 +1,272 @@
+﻿using Assignment5.Helper;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestsAssignment5.Helper
+{
+    class ParserTest
+    {
+
+        [Test]
+        public void NullOrEmptyStringThrowsANE()
+        {
+            // Assemble
+            string input = "";
+            // Act
+            // Assert
+            Assert.That(() => Parser.ParseInputToString(input),
+                Throws.Exception.TypeOf<ArgumentNullException>());
+            // Act
+            input = "   ";
+            // Assert
+            Assert.That(() => Parser.ParseInputToString(input),
+                Throws.Exception.TypeOf<ArgumentNullException>());
+            // Act
+            // Assert
+            Assert.That(() => Parser.ParseInputToString(null),
+                Throws.Exception.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void AllUpperCaseReturnsLowerCase()
+        {
+            // Assemble
+            string input = "TESTE CASE";
+            string expected = input.ToLower();
+            // Act
+            string actual = Parser.ParseInputToString(input);
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        
+        [Test]
+        public void RandomUpperCaseReturnsLowerCase()
+        {
+            // Assemble
+            string input = "TEst casE";
+            string expected = input.ToLower();
+            // Act
+            string actual = Parser.ParseInputToString(input);
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+
+        [Test]
+        public void ConvertInputToInteger()
+        {
+            // Assemble
+            string input = "1234";
+            int expected = 1234;
+            // Act
+            int actual = Parser.ParseToInt(input);
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+        
+        [Test]
+        public void NegativeIntegerThrowsException()
+        {
+            // Assemble
+            string input = "-1234";
+            // Act & Assert
+            Assert.Throws<FormatException>(() => Parser.ParseToInt(input));
+        }
+
+        [Test]
+        public void DoubleThrowsException()
+        {
+            // Assemble
+            string input = "12.34";
+            // Act & Assert
+            Assert.That(() => Parser.ParseToInt(input), Throws.Exception.TypeOf<FormatException>());
+        }
+
+        [Test]
+        public void ElevenDigitIntegerThrowsException()
+        {
+            // Assemble
+            string input = "444444444444";
+            // Act & Assert
+            Assert.That(() => Parser.ParseToInt(input), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void MenuSelectsA()
+        {
+            // Assemble
+            string input = "a";
+            int expected = 1;
+            // Act
+            int actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            
+            // Assemble
+            input = "1";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "1-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "a-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "create-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "new";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void MenuSelectsB()
+        {
+            // Assemble
+            string input = "b";
+            int expected = 2;
+            // Act
+            int actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+
+            // Assemble
+            input = "2";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "2-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "b-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "existing-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "display";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void MenuSelectsC()
+        {
+            // Assemble
+            string input = "c";
+            int expected = 3;
+            // Act
+            int actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+
+            // Assemble
+            input = "3";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "3-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "c-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "edit-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "change";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+
+        [Test]
+        public void MenuSelectsD()
+        {
+            // Assemble
+            string input = "d";
+            int expected = 4;
+            // Act
+            int actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+
+            // Assemble
+            input = "4";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "4-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "d-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "exit";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assemble
+            input = "exit-";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+            // Assemble
+            input = "adios";
+            // Act
+            actual = Parser.ParseInputToMenu(input);
+            // Assert
+            Assert.That(expected, Is.EqualTo(actual));
+        }
+    }
+}
