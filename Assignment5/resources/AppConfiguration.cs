7242b2ef19c20358671a7d27d8777d9439302733
GusRodrigues <omykronbr@gmail.com>
2019-11-24 20:23:34 -0500

    Project changes

---
+++ Assignment5/resources/AppConfiguration.cs
@@ -0,0 +1,22 @@
+﻿/* Assignment 5
+ * 
+ * Revision History
+ *      Gustavo Bonifacio Rodrigues, 2019.11.23: Created
+ */
+using System;
+
+namespace Assignment5.Resources
+{
+    /// <summary>
+    /// General setup of the app
+    /// </summary>
+    public static class AppConfiguration
+    {
+        public static void DefaultApp()
+        {
+            Console.Title = "Assignment 5 by Gustavo Bonifacio Rodrigues";
+            Console.CursorSize = 100;
+            Console.Clear();
+        }
+    }
+}
